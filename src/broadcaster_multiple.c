68810250916cef1ba96895a7f49d2949f0895171
sandaruwanjayaweera <sandaruwangayantha@gmail.com>
2023-05-16 15:14:26 +0300

    print bit32

--- src/broadcaster_multiple.c
+++ src/broadcaster_multiple.c
@@ -299,7 +299,7 @@ static void scan_recv(const struct bt_le_scan_recv_info *info,
 				}
 			}
 
-			printk("err_cnt %d total %d \n", err_cnt, cur_longitude);
+			printk("err_cnt %d total %d ---- lat %d \n", err_cnt, cur_longitude.bit_32, cur_lattitude.bit_32);
 			prev_longitude.bit_32 = cur_longitude.bit_32;
 			// printk("__buf___ %u ___\n", bc_ref_pos_lattitude.bit_32);
 		}

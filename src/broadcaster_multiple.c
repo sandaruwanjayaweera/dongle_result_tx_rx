7813e85331b617cd4206b46be1e5e0572a877a98
sandaruwanjayaweera <sandaruwangayantha@gmail.com>
2023-05-17 16:04:40 +0300

    scanstop_broadcast

--- src/broadcaster_multiple.c
+++ src/broadcaster_multiple.c
@@ -14,7 +14,7 @@
 
 #include <zephyr/drivers/uart.h>
 
-int err_cnt = 0;
+uint32_t err_cnt = 0;
 bc_ref_pos_lattitude cur_lattitude;
 bc_ref_pos_longitude cur_longitude;
 bc_ref_pos_longitude prev_longitude;
@@ -293,13 +293,55 @@ static void scan_recv(const struct bt_le_scan_recv_info *info,
 			cur_longitude.bit_8[1] = buf->data[17];
 			cur_longitude.bit_8[0] = buf->data[18];
 
-			if(cur_lattitude.bit_32 > 600){
+			if(cur_lattitude.bit_32 > 600){ 	// 10 Hz x 60s = 1 min
 				if(cur_longitude.bit_32 - prev_longitude.bit_32 > 1){
 					err_cnt += cur_longitude.bit_32 - prev_longitude.bit_32;
 				}
 			}
 
-			printk("err_cnt %d total %d ---- lat %d \n", err_cnt, cur_longitude.bit_32, cur_lattitude.bit_32);
+
+			if(cur_longitude.bit_32 > 600){ 	// 1 min delay
+
+				int err;	
+				err = bt_le_scan_stop();
+				if (err) {
+					printk("Stopping scanning failed (err %d)\n", err);
+					return err;
+				}
+
+				mfg_data[7] = 0xff; 		// station ID
+
+				mfg_data[13] = cur_longitude.bit_8[3]; 	// longitude
+				mfg_data[14] = cur_longitude.bit_8[2];
+				mfg_data[15] = cur_longitude.bit_8[1];
+				mfg_data[16] = cur_longitude.bit_8[0];
+
+				mfg_data[224] = (uint8_t)(err_cnt >>  24); 	// error number
+				mfg_data[225] = (uint8_t)(err_cnt >>  16);
+				mfg_data[226] = (uint8_t)(err_cnt >>  8);
+				mfg_data[227] = (uint8_t)(err_cnt >>  0);
+
+				while(true){
+					err = bt_le_ext_adv_set_data(adv, ad, ARRAY_SIZE(ad), NULL, 0);
+					if (err) {
+						printk("Failed to set advertising data for set (err %d)\n", err);
+					}
+
+					err = bt_le_ext_adv_start(adv, BT_LE_EXT_ADV_START_DEFAULT);
+					if (err) {
+						printk("Failed to start extended advertising set (err %d)\n", err);
+					}
+
+					k_sleep(K_MSEC(90)); 	// stable 100 ms
+
+					bt_le_ext_adv_stop(adv);
+					if (err) {
+						printk("Advertising failed to stop (err %d)\n", err);
+					}
+				}
+			}
+
+			// printk("err_cnt %d total %d ---- lat %d \n", err_cnt, cur_longitude.bit_32, cur_lattitude.bit_32);
 			prev_longitude.bit_32 = cur_longitude.bit_32;
 			// printk("__buf___ %u ___\n", bc_ref_pos_lattitude.bit_32);
 		}
